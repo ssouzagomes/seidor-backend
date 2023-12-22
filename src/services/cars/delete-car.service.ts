76588d4dcc8d07ab40446cffa52a8d57a2821c20
Samuel Souza <ssouza.gomes10@gmail.com>
2023-12-21 23:32:27 -0300

    fix: verify if car or driver has an open registry before delete

--- src/services/cars/delete-car.service.ts
+++ src/services/cars/delete-car.service.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import { Car, CarTypes } from "../../types/car.types";
 import AppError from "../../exceptions/generic.exception";
 import { idValidation } from "../../validations/generic.validation";
+import { CarUsage } from "../../types/car-usage.types";
 
 export namespace DeleteCarService {
   export const execute = async (model: CarTypes.DeleteParams) => {
@@ -18,6 +19,16 @@ export namespace DeleteCarService {
 
     if (index === -1) {
       throw new AppError('CAR_NOT_FOUND', 404)
+    }
+
+    const carUsage = (data.carUsage as CarUsage[]) || [];
+
+    const carIsInUse = carUsage.find(
+      (usage) => usage.car_id === id && !usage.end_date
+    );
+
+    if (carIsInUse) {
+      throw new AppError("CAR_IS_IN_USE", 400);
     }
 
     cars.splice(index, 1)
--- src/services/drivers/delete-driver.service.ts
+++ src/services/drivers/delete-driver.service.ts
@@ -1,38 +1,49 @@
 import fs from "fs";
-import path from 'path'
+import path from "path";
 import { Driver, DriverTypes } from "../../types/driver.types";
 import AppError from "../../exceptions/generic.exception";
 import { idValidation } from "../../validations/generic.validation";
+import { CarUsage } from "../../types/car-usage.types";
 
 export namespace DeleteDriverService {
   export const execute = async (model: DriverTypes.DeleteParams) => {
     const { id } = await idValidation.parseAsync(model);
 
-    const filePath = path.resolve('./src/database', 'data.json');
-    
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    const filePath = path.resolve("./src/database", "data.json");
 
-    const drivers = data.drivers as Driver[] || []
+    const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
-    const index = drivers.findIndex((driver) => driver.id === id)
+    const drivers = (data.drivers as Driver[]) || [];
+
+    const index = drivers.findIndex((driver) => driver.id === id);
 
     if (index === -1) {
-      throw new AppError('DRIVER_NOT_FOUND', 404)
+      throw new AppError("DRIVER_NOT_FOUND", 404);
     }
 
-    drivers.splice(index, 1)
+    const carUsage = (data.carUsage as CarUsage[]) || [];
+
+    const driverIsUsingACar = carUsage.find(
+      (usage) => usage.driver_id === id && !usage.end_date
+    );
+
+    if (driverIsUsingACar) {
+      throw new AppError("DRIVER_IS_USING_A_CAR", 400);
+    }
+
+    drivers.splice(index, 1);
 
     const newData = {
       ...data,
-      drivers
-    }
+      drivers,
+    };
 
-    const jsonData = JSON.stringify(newData, null, 2); 
+    const jsonData = JSON.stringify(newData, null, 2);
 
     fs.writeFile(filePath, jsonData, (err) => {
       if (err) throw err;
     });
 
-    return { message: 'DRIVER_DELETED' }
+    return { message: "DRIVER_DELETED" };
   };
 }
